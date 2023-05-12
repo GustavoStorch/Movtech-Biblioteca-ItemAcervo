777fb8b279cdfd1366a1bead82886018b3be7ffb
Gustavo Kauan <gustavokauan2002@hotmail.com>
2023-05-12 10:39:43 -0300

    no message

--- CadastroItemDoAcervo/ItemAcervoDAO.cs
+++ CadastroItemDoAcervo/ItemAcervoDAO.cs
@@ -18,7 +18,42 @@ namespace CadastroItemDoAcervo
 
         public void Salvar(ItemAcervoModel itemAcervo)
         {
-
+            using (SqlCommand command = Connection.CreateCommand())
+            {
+                SqlTransaction t = Connection.BeginTransaction();
+                try
+                {
+                    StringBuilder sql = new StringBuilder();
+                    sql.AppendLine($"INSERT INTO mvtBibItemAcervo(codLocal, numExemplar, nome, codAutor," +
+                        $"nomeAutor, codEditora, nomeEditora, tipoItem, nomeLocal, volume, anoEdicao, localizacao," +
+                        $"codSecao, secao, idioma) VALUES(@codLocal, @numExemplar, @nome, @codAutor, @nomeAutor, @codEditora," +
+                        $"@nomeEditora, @tipoItem, @nomeLocal, @volume, @anoEdicao, @localizacao, @codSecao, @secao, @idioma)");
+                    command.CommandText = sql.ToString();
+                    command.Parameters.Add(new SqlParameter("@codLocal", itemAcervo.CodLocal));
+                    command.Parameters.Add(new SqlParameter("@numExemplar", itemAcervo.CodLocal));
+                    command.Parameters.Add(new SqlParameter("@nome", itemAcervo.CodLocal));
+                    command.Parameters.Add(new SqlParameter("@codAutor", itemAcervo.CodLocal));
+                    command.Parameters.Add(new SqlParameter("@nomeAutor", itemAcervo.CodLocal));
+                    command.Parameters.Add(new SqlParameter("@codEditora", itemAcervo.CodLocal));
+                    command.Parameters.Add(new SqlParameter("@nomeEditora", itemAcervo.CodLocal));
+                    command.Parameters.Add(new SqlParameter("@tipoItem", itemAcervo.CodLocal));
+                    command.Parameters.Add(new SqlParameter("@nomeLocal", itemAcervo.CodLocal));
+                    command.Parameters.Add(new SqlParameter("@volume", itemAcervo.CodLocal));
+                    command.Parameters.Add(new SqlParameter("@anoEdicao", itemAcervo.CodLocal));
+                    command.Parameters.Add(new SqlParameter("@localizacao", itemAcervo.CodLocal));
+                    command.Parameters.Add(new SqlParameter("@codSecao", itemAcervo.CodLocal));
+                    command.Parameters.Add(new SqlParameter("@secao", itemAcervo.CodLocal));
+                    command.Parameters.Add(new SqlParameter("@idioma", itemAcervo.CodLocal));
+                    command.Transaction = t;
+                    command.ExecuteNonQuery();
+                    t.Commit();
+                }
+                catch (Exception ex)
+                {
+                    t.Rollback();
+                    throw ex;
+                }
+            }
         }
 
         public void Editar(ItemAcervoModel itemAcervo)
