a348baf82ec6571a5f2fa2299b5dd97897eee353
Gustavo Kauan <gustavokauan2002@hotmail.com>
2023-05-22 12:00:02 -0300

    Iniciando o desenvolvimento da tela de busca de local

---
+++ CadastroItemDoAcervo/FormBuscaLocal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CadastroItemDoAcervo
+{
+    public partial class FormBuscaLocal : Form
+    {
+        public FormBuscaLocal()
+        {
+            InitializeComponent();
+        }
+    }
+}
